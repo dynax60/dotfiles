ad7a052b985abd561753ecb1d054b804ebb7bfff
Denis S.Davydov <d.davydov@autoconnex.ru>
2026-04-03 09:50:30 +0300

    fix: zsh/env.zsh

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,3 +2,6 @@ export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
 export EDITOR=vim
 
 export UV_NATIVE_TLS=1
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
