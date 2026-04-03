0d63b49a3c5fd51df470fd3aace322ab7b458ae3
Denis S.Davydov <d.davydov@autoconnex.ru>
2026-04-03 10:04:24 +0300

    refactor: cleanup rc files

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,19 +1,5 @@
 alias n=nvim
 
-eval "$(uvx --generate-shell-completion zsh)"
-
-# Bat
-alias bathelp='batcat --plain --language=help'
-help() {
-    "$@" --help 2>&1 | bathelp
-}
-
-batdiff() {
-    git diff --name-only --relative --diff-filter=d | xargs batcat --diff
-}
-
-export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
-
 # Password generator
 pwgen() {
   local len=${1:-20}
